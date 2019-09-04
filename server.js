27300b83996931f2f18e9cd3e231e6b00c3a988f
Thiago Lanza <thlanza@gmail.com>
2019-09-04 14:14:07 -0300

    first

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const schema = require('./schema/schema')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql: true
+}))
+
+app.listen(4000, () => {
+    console.log('Listening')
+})
+
+
+
